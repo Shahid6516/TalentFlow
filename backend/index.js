99411854956ae034b463769f7090973b9d4061c4
Mohammad Shahid <www.shahid6516@gmail.com>
2026-04-08 22:13:28 +0530

    Update server listen address to '0.0.0.0' for external access

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/application", applicationRoute);
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT,'0.0.0.0',()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
 })
